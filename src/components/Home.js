60499c80d99c40b82816ef6a5549a1f25dfd47ab
saravanan642 <saravanansenthil605@gmail.com>
2025-10-07 20:23:48 +0530

    images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,47 @@
 import React from 'react'
 import slider1 from '../assests/slider1.jpg'
+import slider2 from "../assests/slider2.jpg"
 
 
 const Home = () => {
   return (
     <div>
+      <div className='flex flex-col'> 
+        <div className=' relative'>
+          <div className=" text-center  mb-1  absolute top-[500px] right-[65%]   font-bold  z-10">
+            <p className='  border border-none p-[3px]'>LET'S MAKE YOUR LIFE HAPPIER</p>
+            <h1 className=' border border-none p-[3px] text-5xl '>Healthy Living</h1>
+            <button className=' border border-solid m-5 p-4 bg-green-700 rounded-[20px]'>Meet Our Docters</button>
+          </div>
+          <img src={slider1} alt="slider " className=" w-full h-[90vh] z-8 object-cover"></img>
+        </div>
 
-      <div className=" text-center  mb-1  absolute top-[500px] right-[65%]   font-bold  z-10">
-        <p className='  border border-none p-[3px]'>LET'S MAKE YOUR LIFE HAPPIER</p>
-        <h1 className=' border border-none p-[3px] text-5xl '>Healthy Living</h1>
-        <button className=' border border-solid m-5 p-4 bg-green-700 rounded-[20px]'>Meet Our Docters</button>
+        <div className=' relative'>
+          <div className=" text-center  mb-1  absolute top-[500px] right-[65%]   font-bold  z-10">
+            <p className='  border border-none p-[3px]'>LET'S MAKE YOUR LIFE HAPPIER</p>
+            <h1 className=' border border-none p-[3px] text-5xl '>Healthy Living</h1>
+            <button className=' border border-solid m-5 p-4 bg-green-700 rounded-[20px]'>Meet Our Docters</button>
+          </div>
+          <img src={slider2} alt="slider " className=" w-full h-[90vh] z-8 object-cover"></img>
+
+        </div>
+
+
+       < div className=' relative'>
+          <div className=" text-center  mb-1  absolute top-[500px] right-[65%]   font-bold  z-10">
+            <p className='  border border-none p-[3px]'>LET'S MAKE YOUR LIFE HAPPIER</p>
+            <h1 className=' border border-none p-[3px] text-5xl '>Healthy Living</h1>
+            <button className=' border border-solid m-5 p-4 bg-green-700 rounded-[20px]'>Meet Our Docters</button>
+          </div>
+          <img src={slider2} alt="slider " className=" w-full h-[90vh] z-8 object-cover"></img>
+
+        </div>
 
 
       </div>
-      <img src={slider1} alt="slider " className=" w-full h-[90vh] z-8"></img>
-
 
     </div>
+
   )
 }
 
