019f4e8f5a7c16e66e29ff251aa3eade0d578fb4
saravanan642 <saravanansenthil605@gmail.com>
2025-10-09 12:50:00 +0530

    about update

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,13 @@ import slider4 from "../assests/slider4.jpg"
 const About = () => {
   return (
     <div>
-       <img src={slider4} alt="slider " className=' border border-none m-[30px] w-[95%] h-[700px] overflow-x-hidden'></img>
-       <p>wknwjhvjdvqdufewj ejbjqebjb</p>
+      <div className=" text-center  mb-1  absolute top-[1500px] right-[65%]   font-bold  z-10">
+        <p className='  border border-none p-[3px]'>LET'S MAKE YOUR LIFE HAPPIER</p>
+        <h1 className=' border border-none p-[3px] text-5xl '>Healthy Living</h1>
+        <button className=' border border-solid m-5 p-4 bg-green-700 rounded-[20px]'>Meet Our Docters</button>
+      </div>
+      <img src={slider4} alt="slider " className=' border border-none m-[30px] w-[95%] h-[700px] overflow-x-hidden'></img>
+      <p></p>
     </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div>
       <div className='flex flex-col'> 
-        <div className=' relative'>
+        <div className='  sticky'>
           <div className=" text-center  mb-1  absolute top-[500px] right-[65%]   font-bold  z-10">
             <p className='  border border-none p-[3px]'>LET'S MAKE YOUR LIFE HAPPIER</p>
             <h1 className=' border border-none p-[3px] text-5xl '>Healthy Living</h1>
