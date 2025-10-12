1792b4920241d065e2a22f5400e590cf318fdefb
saravanan642 <saravanansenthil605@gmail.com>
2025-10-12 13:02:44 +0530

    about

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ import slider5 from "../assests/slider5.jpg"
 const About = () => {
   return (
     <div className=' '>
-      <div className=" text-center  mb-1  absolute top-[1300px] right-[55%]   font-bold  z-10">
+      <div className=" text-center  mb-1  absolute top-[1300px] right-[50%]   font-bold  z-10">
         <div className=' flex gap-[10px]'>
           <h2 className='border border-none p-[3px] font-bold text-4xl '>Welcome to Your </h2>
           <div className='flex g-[10px]'>
@@ -24,13 +24,13 @@ const About = () => {
           <p>urna mattis. Ut placerat, diam a tempus vehicula.</p>
         </div>
         <div>
-          <div className='flex  border border-none mt-[30px]'>
-            <img src={slider5} alt='slider5'></img>
-
+          <div className='flex  border border-none mt-[60px] ]'>
+            <img src={slider5} alt='slider5' className=' rounded-[40%] w-[20%] h-[20%]  border border-none '></img>
+          
+          <div className=' text-left m-[40px]'>
+            <p  className=' font-bold'>Dr. Neil Jackson</p>
+            <p className=' font-serif'>General Principal</p>
           </div>
-          <div className=' border border-none '>
-            <p>Dr. Neil Jackson</p>
-            <p>General Principal</p>
           </div>
         </div>
       </div>
