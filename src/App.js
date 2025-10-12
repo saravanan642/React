d86c2f6e848e4cf3cd10838ec450c3b718c594ba
saravanan642 <saravanansenthil605@gmail.com>
2025-10-12 15:52:22 +0530

    home

--- src/App.js
+++ src/App.js
@@ -12,9 +12,11 @@ function App() {
         <Header />
 
         <Routes>
-          
-          <Route path="/home" element={<Home />} />
+
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/home" element={<Home />} />
+          
         </Routes>
 
         <Footer />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import slider1 from '../assests/slider1.jpg'
 import slider2 from "../assests/slider2.jpg"
+import About from './About'
 
 
 const Home = () => {
@@ -8,7 +9,7 @@ const Home = () => {
     <div>
       <div className='flex flex-col'> 
         <div className='  sticky'>
-          <div className=" text-center  mb-1  absolute top-[500px] right-[65%]   font-bold  z-10">
+          <div className=" text-center  mb-1  absolute top-[500px] right-[65%]   font-bold  ">
             <p className='  border border-none p-[3px]'>LET'S MAKE YOUR LIFE HAPPIER</p>
             <h1 className=' border border-none p-[3px] text-5xl '>Healthy Living</h1>
             <button className=' border border-solid m-5 p-4 bg-green-700 rounded-[20px]'>Meet Our Docters</button>
@@ -17,6 +18,7 @@ const Home = () => {
         </div>
 
       
+<About/>
 
 
 
