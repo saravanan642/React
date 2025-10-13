93dec55b603ea1fa561017e991c513181843b395
saravanan642 <saravanansenthil605@gmail.com>
2025-10-13 21:47:52 +0530

    explore

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import slider1 from '../assests/slider1.jpg'
 import slider2 from "../assests/slider2.jpg"
 import About from './About'
@@ -7,24 +8,30 @@ import About from './About'
 const Home = () => {
   return (
     <div>
-      <div className='flex flex-col'> 
-        <div className='  sticky'>
-          <div className=" text-center  mb-1  absolute top-[500px] right-[65%]   font-bold  ">
-            <p className='  border border-none p-[3px]'>LET'S MAKE YOUR LIFE HAPPIER</p>
-            <h1 className=' border border-none p-[3px] text-5xl '>Healthy Living</h1>
-            <button className=' border border-solid m-5 p-4 bg-green-700 rounded-[20px]'>Meet Our Docters</button>
+      <div>
+        <div>
+          <div className='flex flex-col sticky'>
+
+            <div className=" text-center  mb-1  absolute top-[500px] right-[65%]   font-bold  ">
+              <p className='  border border-none p-[3px]'>LET'S MAKE YOUR LIFE HAPPIER</p>
+              <h1 className=' border border-none p-[3px] text-5xl '>Healthy Living</h1>
+              <div className='border border-none mt-[30px]'>
+                <Link to='/about' className=' border border-solid m-5 p-4 bg-green-700 rounded-[20px]'>Meet Our Docters</Link>
+              </div>
+            </div>
+
           </div>
           <img src={slider1} alt="slider  gap-[20px] " className='h-[1000px]  '></img>
         </div>
 
-      
-<About/>
+
+        <About />
 
 
 
-      </div>
+      </div >
 
-    </div>
+    </div >
 
   )
 }
